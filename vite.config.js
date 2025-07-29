6481bd5b4173714372d3cba19744557e35965ae5
Quentin Gliech <quentingliech@gmail.com>
2025-07-29 09:37:49 +0200

    Update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
 import { resolve } from "node:path";
 import tailwindcss from "@tailwindcss/vite";
 import { cloudflare } from "@cloudflare/vite-plugin";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import viteReact from "@vitejs/plugin-react-oxc";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [
-    TanStackRouterVite({ autoCodeSplitting: true }),
+    tanstackRouter({ 
+      target: "react",
+      autoCodeSplitting: true, 
+    }),
     viteReact(),
     tailwindcss(),
     cloudflare(),
