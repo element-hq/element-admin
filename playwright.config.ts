66b60fe2f6465c376755d0ca0e7fa735fa5bb386
Quentin Gliech <quentingliech@gmail.com>
2025-09-09 16:17:03 +0200

    Also output HTML report in CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   forbidOnly: !!process.env["CI"],
   retries: process.env["CI"] ? 2 : 0,
   workers: process.env["CI"] ? 1 : undefined,
-  reporter: process.env["CI"] ? "github" : "html",
+  reporter: process.env["CI"] ? [["github"], ["html"]] : "html",
   use: {
     baseURL: process.env["BASE_URL"] || "http://127.0.0.1:4173",
     trace: "on-first-retry",
